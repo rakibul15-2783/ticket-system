e4378371c46b0c4207310e7b199c426914a2b730
rakibul-2783 <rakibcse192@gmail.com>
2023-08-14 18:58:55 +0600

    last updated

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\View;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -44,11 +45,11 @@ Route::get('/logout',[AuthController::class,'logout'])->name('logout');
 
 
 
-View::composer('admin.includes.sidebar', function($view){
-     
-      $tickets = App\Models\Ticket::orderBy('id','desc')->get();
-      $view->with('tickets',$tickets);
-});
+// View::composer('admin.includes.sidebar', function($view){
+
+//       $tickets = App\Models\Ticket::orderBy('id','desc')->get();
+//       $view->with('tickets',$tickets);
+// });
 
 
 
